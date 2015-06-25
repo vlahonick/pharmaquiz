158899d52bd6a8015b7334e028d09e60d79bf447
vlahonick <vlahonick@gmail.com>
2015-06-25 15:07:03 +0300

    CSS fixes

---
+++ web/js/a8ab366_part_2_jquery.tabs.accordion_6.js
@@ -0,0 +1,11 @@
+jQuery(function() {
+  jQuery( "#tabs" ).tabs();
+});
+jQuery(function() {
+  jQuery( ".subtabs" ).tabs();
+});
+jQuery(function() {
+  jQuery( ".accordion" ).accordion({
+    heightStyle: "content"
+  });
+});
